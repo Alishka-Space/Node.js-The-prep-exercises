fb687026228a90a4f9ba33661281f563d53942cc
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2024-07-07 17:23:22 +0200

    add

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -5,6 +5,36 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
+
+//creating a new post
+app.post("/posts", (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("Title and content are required");
+    return;
+  }
+  fs.writeFile(`${title}.txt`, content )
+
+  res.send("ok");
+});
+
+// Reading a post
+
+  app.get("/posts/:title", (req, res) => {
+ const { title } = req.params;
+const hasPost = fs.existsSync(`${title}`);
+if (!hasPost) {
+  const post = fs.readFileSync(`${title}.txt`, "utf-8");
+  res.jason({ title, content: post });
+}else{
+  res.status(404).send("Post not found");
+}  
+  })
+  ////////
+   
+
+
 // YOUR CODE GOES IN HERE
 app.get("/", function (req, res) {
   res.send("Hello World");
@@ -16,47 +46,30 @@ app.get("/", function (req, res) {
   try {
     fs.readFileSync(filePath, content);
     res.send("ok");
+    
   } catch (error) {
     res.status(500);
     res.send("Error writing showing file");
   }
 });
 
-app.post("/blogs", (req, res) => {
-  // How to get the title and content from the request??
-  const { title, content } = req.body;
-  const filePath = path.join(__dirname, `${title}.txt`);
-  try {
-    fs.writeFileSync(filePath, content);
-    res.send("ok");
-  } catch (error) {
-    res.status(500);
-    res.send("Error writing file");
-  }
-});
+// update a post
 app.put("/posts/:title", (req, res) => {
   // How to get the title and content from the request?
   // What if the request does not have a title and/or content?
   const { title } = req.params;
-  const { content } = req.body;
-  if (!content) {
+  const { content } = req.body.content;
+  if (!content || !title)  {
     return res.status(400).send('Content is required');
 }
-  const filePath = path.join(__dirname, `${title}.txt`);
 
-  if (fs.existsSync(filePath)) {
-    try {
-      fs.writeFileSync(filePath, content);
-      res.send("ok");
-    } catch (error) {
-      console.error("Error writing file:", error);
-      res.status(500).send("Error writing file");
-    }
-  } else {
-    res.status(404).send("This post does not exist!");
-  }
-  //URL: http://localhost:3000/posts/My first blog postman testing url
-});
+
+  const updatedBlog = fs.writeFileSync(title, content);
+  res.send(updatedBlog);
+})
+
+
+  
 app.delete('/blogs/:title', (req, res) => {
   // How to get the title from the url parameters?
   const { title } = req.params;
@@ -71,7 +84,7 @@ app.delete('/blogs/:title', (req, res) => {
             res.status(500).send("Error deleting file");
         }
     } else {
-        res.status(404).send('This post does not exist!');
+        res.status(404).send('This post does not Found!');
     }
 })
 
